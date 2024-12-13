e5041382c35745ad754c7d7c0dee574910b8f996
ycs-202007034 <heesup9683@naver.com>
2024-12-13 15:30:03 +0900

    group 라우팅 설정

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const connectDB = require("./config/db");
-const scheduleRoutes = require("./routes/scheduleRoutes");
-const loginRoutes = require("./routes/login"); // 로그인 라우터 파일 이름 변경
-const updateProfileRoutes = require("./routes/updateProfile");
+const scheduleRoutes = require("./routes/scheduleRoutes"); // 사용자 시간표 API
+const groupRoutes = require("./routes/groupRoutes"); // 그룹 API 추가
+const loginRoutes = require("./routes/login"); // 로그인 라우트
+const updateProfileRoutes = require("./routes/updateProfile"); // 프로필 업데이트 라우트
 
 const app = express();
 
@@ -18,7 +19,7 @@ app.use(express.json());
 
 // API 키 인증 미들웨어
 app.use((req, res, next) => {
-  const clientApiKey = req.headers['x-api-key'];
+  const clientApiKey = req.headers["x-api-key"];
   const serverApiKey = process.env.SERVER_API_KEY;
 
   if (!clientApiKey || clientApiKey !== serverApiKey) {
@@ -29,9 +30,10 @@ app.use((req, res, next) => {
 });
 
 // 라우팅 설정
-app.use("/api/schedules", scheduleRoutes); // 공강 시간표 API
+app.use("/api/schedules", scheduleRoutes); // 사용자 시간표 API
+app.use("/api/groups", groupRoutes); // 그룹 API 추가
 app.use("/api/auth", loginRoutes); // 로그인 및 회원가입 라우트
-app.use("/api/profile", updateProfileRoutes); // 프로필 업데이트 라우트 추가
+app.use("/api/profile", updateProfileRoutes); // 프로필 업데이트 라우트
 
 // 서버 포트 설정
 const PORT = process.env.PORT || 5000;
